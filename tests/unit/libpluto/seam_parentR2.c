b26b1e570218cb6143b6d1a82ee176f015d10b91
Michael Richardson <mcr@credil.org>
2018-02-22 16:01:32 -0500

    wo#6839 . refactor parentR2 to add ikev1 receive

--- tests/unit/libpluto/seam_parentR2.c
+++ tests/unit/libpluto/seam_parentR2.c
@@ -1,5 +1,13 @@
 #include "pluto_crypt.h"
 
+void update_ngi(struct pcr_kenonce *kn)
+{
+  /* now fill in the KE values from a constant.. not calculated */
+  clonetowirechunk(&kn->thespace, kn->space, &kn->n,   tc14_nr, tc14_nr_len);
+  clonetowirechunk(&kn->thespace, kn->space, &kn->gi,  tc14_gr, tc14_gr_len);
+  clonetowirechunk(&kn->thespace, kn->space, &kn->secret, tc14_secretr,tc14_secretr_len);
+}
+
 /* this is replicated in the unit test cases since the patching up of the crypto values is case specific */
 void recv_pcap_packet(u_char *user
 		      , const struct pcap_pkthdr *h
@@ -15,10 +23,7 @@ void recv_pcap_packet(u_char *user
     if(st) {
         st->st_connection->extra_debugging = DBG_EMITTING|DBG_CONTROL|DBG_CONTROLMORE;
 
-        /* now fill in the KE values from a constant.. not calculated */
-        clonetowirechunk(&kn->thespace, kn->space, &kn->n,   tc14_nr, tc14_nr_len);
-        clonetowirechunk(&kn->thespace, kn->space, &kn->gi,  tc14_gr, tc14_gr_len);
-
+        update_ngi(kn);
         run_one_continuation(crypto_req);
     }
 }
@@ -36,10 +41,25 @@ void recv_pcap_packet2(u_char *user
     st = state_with_serialno(1);
     if(st) {
       st->st_connection->extra_debugging = DBG_PRIVATE|DBG_CRYPT|DBG_PARSING|DBG_EMITTING|DBG_CONTROL|DBG_CONTROLMORE;
-      clonetowirechunk(&kn->thespace, kn->space, &kn->secret, tc14_secretr,tc14_secretr_len);
     }
 
     run_one_continuation(crypto_req);
+}
+
+void recv_pcap_packet1ikev1(u_char *user
+                      , const struct pcap_pkthdr *h
+                      , const u_char *bytes)
+{
+    struct state *st;
+    struct pcr_kenonce *kn = &crypto_req->pcr_d.kn;
+
+    recv_pcap_packet_gen(user, h, bytes);
+
+    /* find st involved */
+    st = state_with_serialno(1);
+    if(st) {
+      st->st_connection->extra_debugging = DBG_PRIVATE|DBG_CRYPT|DBG_PARSING|DBG_EMITTING|DBG_CONTROL|DBG_CONTROLMORE;
+    }
 }
 
 #ifndef PCAP_INPUT_COUNT
