cb6f0321bbe4b48a2fc6e83511d19540da3e9984
Michael Richardson <mcr@credil.org>
2018-02-27 20:29:33 -0500

    wo#6839 . remove dead cruft

--- tests/unit/libpluto/seam_ikev1_crypto.c
+++ tests/unit/libpluto/seam_ikev1_crypto.c
@@ -14,21 +14,3 @@ stf_status start_dh_secret(struct pluto_crypto_req_cont *cn
     return STF_SUSPEND;
 }
 
-#if 0
-void finish_dh_secretiv(struct state *st,
-			struct pluto_crypto_req *r)
-{
-  /* XXX maybe should do something ... */
-
-  st->hidden_variables.st_skeyid_calculated = TRUE;
-}
-
-stf_status start_dh_secretiv(struct pluto_crypto_req_cont *cn
-			     , struct state *st
-			     , enum crypto_importance importance
-			     , enum phase1_role init       /* TRUE=g_init,FALSE=g_r */
-			     , u_int16_t oakley_group2)
-{
-  return STF_SUSPEND;
-}
-#endif
