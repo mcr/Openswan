a28c4a72193371b52329bf7752cc044b1479aa13
Michael Richardson <mcr@credil.org>
2018-03-05 22:13:53 -0500

    wo#6839 . log what events are added to debug IKEv1 timer issues

--- tests/unit/libpluto/seam_timer.c
+++ tests/unit/libpluto/seam_timer.c
@@ -1,11 +1,20 @@
+#ifndef WANT_TIMER
 /* timer.c SEAM */
 void timer_list(void) {}
 
-void event_schedule(enum event_type type, time_t tm, struct state *st) { }
+void event_schedule(enum event_type type, time_t tm, struct state *st) {
+  if (st == NULL)
+    DBG_log("inserting event %s, timeout in %lu second"
+            , enum_show(&timer_event_names, type), (unsigned long)tm);
+  else
+    DBG_log("inserting event %s, timeout in %lu seconds for #%lu"
+            , enum_show(&timer_event_names, type), (unsigned long)tm
+            , st->st_serialno);
+}
 void _delete_dpd_event(struct state *st, const char *file, int lineno) {}
 void delete_event(struct state *st) {}
 
-
+#endif
 
 
 
